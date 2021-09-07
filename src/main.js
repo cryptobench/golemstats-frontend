5fa0bbab50d148d68e5e5dd2ed5a32b402f430ad
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-09-07 14:37:09 +0200

    Remove sentry

--- src/main.js
+++ src/main.js
@@ -1,43 +1,29 @@
-import Vue from 'vue'
-import { ToastPlugin, ModalPlugin } from 'bootstrap-vue'
-import VueCompositionAPI from '@vue/composition-api'
-import VueGtag from 'vue-gtag'
+import Vue from "vue"
+import { ToastPlugin, ModalPlugin } from "bootstrap-vue"
+import VueCompositionAPI from "@vue/composition-api"
+import VueGtag from "vue-gtag"
 
-import router from './router'
-import store from './store'
-import App from './App.vue'
-import VueMeta from 'vue-meta'
+import router from "./router"
+import store from "./store"
+import App from "./App.vue"
+import VueMeta from "vue-meta"
 Vue.use(VueMeta)
 
-import * as Sentry from '@sentry/vue'
-import { Integrations } from '@sentry/tracing'
-
-Sentry.init({
-  Vue,
-  dsn:
-    'https://5cdd1cfb700642739080ffb51c71e41f@o776625.ingest.sentry.io/5797498',
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-})
 // Configuration VueAnalytics
 
 Vue.use(
   VueGtag,
   {
-    config: { id: 'G-P8PVPYRZSY' },
+    config: { id: "G-P8PVPYRZSY" },
   },
   router
 )
 // Global Components
-import './global-components'
+import "./global-components"
 
 // 3rd party plugins
-import '@/libs/portal-vue'
-import '@/libs/toastification'
+import "@/libs/portal-vue"
+import "@/libs/toastification"
 
 // BSV Plugin Registration
 Vue.use(ToastPlugin)
@@ -47,10 +33,10 @@ Vue.use(ModalPlugin)
 Vue.use(VueCompositionAPI)
 
 // import core styles
-require('@core/scss/core.scss')
+require("@core/scss/core.scss")
 
 // import assets styles
-require('@/assets/scss/style.scss')
+require("@/assets/scss/style.scss")
 
 Vue.config.productionTip = false
 
@@ -58,4 +44,4 @@ new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app')
+}).$mount("#app")
